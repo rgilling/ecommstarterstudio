1ce76f9ff261fd1f7a5ed8a291877114ae441df2
Richard Gilling <Richard.Gilling@tryzens.com>
2022-12-15 10:47:13 +1100

    Initial Studio Commit

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,68 @@
-export const schemaTypes = []
+export const schemaTypes = [
+  {
+    title: "Ad - Banner Ad",
+    name: "landingPageBannerAd",
+    type: "document",
+    groups: [
+      { name: "banner", title: "Banner" },
+      { name: "cta", title: "Call to Action" },
+      { name: "meta", title: "Metadata"},
+    ],
+    fields: [
+      {
+        title: "Name",
+        name: "name",
+        type: "string",
+        description: "The name of the banner, used only in the management UI.",
+        group: "meta"
+      },
+      {
+        title: "Image",
+        name: "image",
+        type: "image",
+        description: "The image to be used in the banner",
+        group: "banner"
+      },
+      {
+        title: "Leading Line",
+        name: "leadingLine",
+        type: "string",
+        description: "Can be used as the main headline in the banner.",
+        grouop: "banner"
+      },
+      {
+        title: "Intro Text",
+        name: "introText",
+        type: "string",
+        description: "Can be used as the main headline in the banner.",
+        group: "banner"
+      },
+      {
+        title: "Link",
+        name: "link",
+        type: "url",
+        validation: Rule => Rule.uri({
+          scheme: ['http', 'https'],
+          allowRelative: true
+        }),
+        group: "cta",
+      },
+      {
+        title: "Link Type",
+        name: "linkType",
+        type: "string",
+        options: {
+          list: ["internal", "external"],
+        },
+        group: "cta",
+      },
+      {
+        title: "Link Text",
+        name: "linkText",
+        type: "string",
+        initialValue: "Shop Now",
+        group: "cta",
+      }
+    ]
+  }
+]
